970d4aaf8db333fd14d39ae3b106e13888de3cc0
Pekka Nykänen <pekka.nykanen@outlook.com>
2022-07-17 13:28:03 +0300

    Some functions available to all

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -9,14 +9,17 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Service\RestaurantService;
 use App\Service\GroupService;
 use App\Entity\Group;
+use App\Entity\Restaurant;
+use App\Form\AddRestaurantType;
 use Symfony\Component\Security\Core\Security;
 
 class RestaurantController extends AbstractController
 {
     private RestaurantService $restaurantService;
     
-    public function __construct(RestaurantService $restaurantService, Security $security) {
+    public function __construct(RestaurantService $restaurantService, GroupService $groupService, Security $security) {
         $this->restaurantService = $restaurantService;
+        $this->groupService = $groupService;
         
         $this->security = $security;
     }
@@ -27,19 +30,63 @@ class RestaurantController extends AbstractController
 
 
     /**
-     * @Route("/restaurants/", name="resturants")
+     * @Route("/restaurants/", name="restaurants")
      */
     public function restaurants(): Response
     {
         $usersGrp = $this->security->getUser()->getGrp();
+        $available_restaurants = $this->restaurantService->findNotIn($usersGrp);
+        /*
         if ($usersGrp == null){
             $group_restaurants = [];
         }
         else {
         $group_restaurants = $this->restaurantService->findIn($usersGrp);
         }
-        
+        */
+
         return $this->renderForm('restaurants/restaurant.html.twig', [
-            'restaurants' => $group_restaurants,
-        ]);    }
+            'available' => $available_restaurants,
+            'group' => $usersGrp
+        ]);    
+    }
+
+    /**
+     * @Route("/restaurants/add", name="add_restaurant_public")
+     */
+    public function add_restaurant_public(Request $request): Response
+    {
+        $new_restaurant = new Restaurant();
+        $form = $this->createForm(AddRestaurantType::class, $new_restaurant);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $new_restaurant->setName($form->getData()->getName());
+            $new_restaurant->setOpens($form->getData()->getOpens());
+            $new_restaurant->setUrl($form->getData()->getUrl());
+            $this->restaurantService->add($new_restaurant);
+            return $this->redirectToRoute('add_restaurant_public');
+        }
+        $restaurants = $this->restaurantService->findAll();
+
+        return $this->renderForm('restaurants/restaurant_add.html.twig', [
+            'form' => $form,
+            'restaurants' => $restaurants
+        ]);
+    }
+
+    /**
+     * @Route("/restaurants/group/{id}/restaurants/{r_id}/add/", name="add_r_to_group_public")
+     */
+    public function add_r_to_group(int $id,int $r_id): Response
+    {
+        $group = $this->groupService->get(intval($id));
+        $restaurant = $this->restaurantService->get(intval($r_id));
+        $group->addRestaurant($restaurant);
+        $restaurant->addGroup($group);
+        $this->groupService->save($group);
+        $this->restaurantService->save($restaurant);
+
+        
+        return $this->redirectToRoute('restaurants');
+    }
 }
